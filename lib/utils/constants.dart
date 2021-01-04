76f0a8aece02e633b48c15b372ba7653a7a68827
ClementSicard <clement.sicard@epfl.ch>
2021-01-04 15:15:23 +0100

    [Automatic commit - update bot] Updated constants to version 1.5.0

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
   static const TEL_CLEMENT = '+33781781494';
-  static const CURRENT_VERSION = '1.4.0';
+  static const CURRENT_VERSION = '1.5.0';
   static const MESSAGERIE = '789';
 }
